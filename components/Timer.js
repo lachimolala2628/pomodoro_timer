86c20e2be15d5f57153eefd712a5eb73afef5cb3
Ayush Kumar <aayushkumar1228@gmail.com>
2025-03-13 01:19:13 +0530

    button design and size change

--- components/Timer.js
+++ components/Timer.js
@@ -14,32 +14,32 @@ const Timer = ({ stage, switchStage, getTickingTime, seconds, ticking, startTime
                 {
                     options.map((options, index) => {
                         return (
-                            <h1 key={index} className={` ${index === stage ? 'text-[#FAFAFA] bg-[#18181B]' : ''} px-3 py-1 cursor-pointer transition-all rounded`} onClick={() => switchStage(index)}>
+                            <h1 key={index} className={` ${index === stage ? 'text-[#FAFAFA] bg-[#18181B]' : ''} px-2 py-1 text-sm cursor-pointer transition-all rounded`} onClick={() => switchStage(index)}>
                                 {options}
                             </h1>
                         )
                     })
                 }
             </div>
-            <div className='my-16'>
+            <div className='my-12'>
                 <h1 className='text-9xl font-bold select-none m-0 text-[#18181B]'>
                     {getTickingTime()}:{seconds.toString().padStart(2, '0')}
                 </h1>
             </div>
-            <div className='flex gap-4 items-center'>
-                <button className='px-16 py-2 text-2xl rounded-md text-[#FAFAFA] tracking-widest bg-[#18181B] font-bold' onClick={startTimer}>
+            <div className='flex flex-col gap-4 items-center'>
+                <button className='px-36 py-2 text-2xl rounded-md text-[#FAFAFA] bg-[#18181B] font-bold' onClick={startTimer}>
                     {ticking ? <BsPause /> : <CiPlay1 />}
                 </button>
                 {isTimeUp && (
                     <GoMute
-                        className="text-3xl cursor-pointer rounded-md text-[#FAFAFA] bg-[#18181B] py-2 px-2"
+                        className="text-4xl cursor-pointer rounded-md text-[#18181B] bg-[#F2EEE3] py-2"
                         onClick={muteAlarm}
                     />
                 )}
             </div>
             {
                 ticking && (
-                    <button onClick={reset} className='rounded-md text-[#FAFAFA] tracking-widest bg-[#18181B] mt-5 px-16 py-2 text-2xl font-bold'>
+                    <button onClick={reset} className='rounded-md text-[#18181B] bg-[#F2EEE3] mt-5 py-2 text-2xl font-bold'>
                         <BsArrowRepeat size={20} />
                     </button>
                 )
